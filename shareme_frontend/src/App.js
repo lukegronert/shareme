0030478782ee70af45e58d30a7327a8c738c1053
Luke Gronert <lukegronert@gmail.com>
2022-11-02 13:30:00 +0800

    Set up react-router-dom. Added components and containers directories.

--- shareme_frontend/src/App.js
+++ shareme_frontend/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './index.css';
+
+import Login from './components/Login';
+import Home from './container/Home';
 
 const App = () => {
   return (
-    <h1 className="pl5">
-      Hello World.
-    </h1>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   )
 }
 
