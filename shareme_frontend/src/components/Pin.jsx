0f0cfa74bd7013f9235f638d979fcf190f4cbc25
Luke Gronert <lukegronert@gmail.com>
2022-11-08 20:42:50 -0800

    Added destination button and delete button to Pin.

--- shareme_frontend/src/components/Pin.jsx
+++ shareme_frontend/src/components/Pin.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Link, Navigate, useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { MdDownloadForOffline } from 'react-icons/md';
-import { AiTwooneDelete } from 'react-icons/ai';
+import { AiTwotoneDelete } from 'react-icons/ai';
 import { BsFillArrowUpRightCircleFill } from 'react-icons/bs';
 
 import { client, urlFor } from '../client'
@@ -34,6 +34,14 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
           window.location.reload();
         })
     }
+  }
+
+  const deletePin = (id) => {
+    client
+      .delete(id)
+      .then(() => {
+        window.location.reload()
+      })
   }
 
     useEffect(() => {
@@ -80,6 +88,31 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
                 </button>
               )}
             </div>
+            <div className="flex justify-between items-center gap-2 w-full">
+              {destination && (
+                <a
+                  href={destination}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 runded-full opacity-70 hover:100 hover:shadow-md"
+                  onClick={(e) => e.stopPropagation()}
+                >
+                  <BsFillArrowUpRightCircleFill />
+                  {destination.length > 20 ? destination.slice(8, 20) : destination.slice(8)}
+                </a>
+              )}
+              {postedBy?._id === user.sub && (
+                <button
+                  onClick={(e) => {
+                    e.stopPropagation()
+                    deletePin(_id)
+                  }}
+                  type="button" className="bg-white p-2 opacity-70 hover:opacity-100 font-bold text-dark text-base rounded-3xl hover:shadow-md outline-none"
+                >
+                  <AiTwotoneDelete />
+                </button>
+              )}
+            </div>
           </div>            
         )}
       </div>
@@ -87,4 +120,4 @@ const Pin = ({ pin: { postedBy, image, _id, destination, save } }) => {
   )
 }
 
-export default Pin
+export default Pin;
